fd66d36dd3aa05171c694a5cefd98ebb32c8a849
Stuart Urback <stuart@urback.net>
2015-11-18 16:31:39 -0700

    Completed functionality

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var myButton = document.getElementById("button");
+console.log(myButton);
+myButton.addEventListener("click", function(){
+  var number = myButton.previousElementSibling.value;
+  number = Number(number);
+  var totalCalc = document.getElementById("tip-calc");
+  var tip = .15 * number;
+  tip = Number(tip);
+  var totalAmt = document.getElementById("total-calc");
+  totalCalc.textContent = tip;
+  totalAmt.textContent = tip + number;
+});
