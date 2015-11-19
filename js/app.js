a88728feb47b8b702e2658893bd5447e66a77953
Stuart Urback <stuart@urback.net>
2015-11-19 15:06:49 -0700

    Updated with stretch.

--- js/app.js
+++ js/app.js
@@ -1,61 +1,41 @@
-var awesome = document.getElementById("awesome");
-var douche = document.getElementById("douche");
-var dece = document.getElementById("dece");
-var poor = document.getElementById("poor");
-
-douche.addEventListener("click", function(){
-  document.getElementById('header').innerHTML = "Really brah?";
-  document.getElementById('main').setAttribute('class', 'red');
-});
+var textInput = document.getElementById("mealcost");
+var highlight = document.getElementById("service");
 
 var calculate = function (price, percent){
   var priceArray =[];
-  var tip = percent*price;
+  var tip = percent * price;
   var total = tip + price;
   priceArray[0] = tip.toFixed(2);
   priceArray[1] = total.toFixed(2);
   return priceArray;
 }
 
-function clickButton (buttonToClick, percent){
-  buttonToClick.addEventListener("click", function(){
-    var number = document.getElementById("mealcost").value;
-    var totalCalc = document.getElementById("tip-calc");
-    if (buttonToClick != douche){
-      document.getElementById('main').removeAttribute("class", "red");
-      document.getElementById('header').innerHTML = "Tip Calculator";
-    }
-    if (number.charAt(0)=== "$"){
-      number = +number.slice(1,number.length);
-    }
-    number = +number;
-    if (!Number(number)){
-      document.getElementById("header").innerHTML = "Please enter numbers only";
-    }else{
-      document.getElementById("tip-calc").textContent = "$ " + calculate(number, percent)[0];
-      document.getElementById("total-calc").textContent = "$ " +  calculate(number, percent)[1];
-    }
-  });
+function addDouche(){
+  document.getElementById('header').innerHTML = "Really brah?";
+  document.getElementById('main').setAttribute('class', 'red');
 }
 
-document.getElementById("mealcost").addEventListener("keypress", function (){
-  // if (event.keyCode == 13){
-    var number = document.getElementById("mealcost").value;
-    var totalCalc = document.getElementById("tip-calc");
-    if (number.charAt(0)=== "$"){
-      number = +number.slice(1,number.length);
-    }
-    number = +number;
-    if (!Number(number)){
-      document.getElementById("header").innerHTML = "Please enter numbers only";
-    }else{
-      document.getElementById("tip-calc").textContent = "$ " + calculate(number, .2)[0];
-      document.getElementById("total-calc").textContent = "$ " +  calculate(number, .2)[1];
-    }
-  // }
-});
+function removeDouche(){
+  document.getElementById('header').innerHTML = "Tip Calculator";
+  document.getElementById('main').removeAttribute('class', 'red');
+}
 
-clickButton(awesome, .20);
-clickButton(douche, 1);
-clickButton(dece, .15);
-clickButton(poor, .1);
+var printScreen = function (){
+  var cost = +document.getElementById("mealcost").value;
+  var tip = +document.getElementById("service").value;
+  if (!Number(cost) && cost != ""){
+        document.getElementById('header').textContent = "Please use only numbers";
+  }else{
+      document.getElementById('header').textContent = "Tip Calculator";
+      document.getElementById("tip-calc").textContent = "$ " + calculate(cost, tip)[0];
+      document.getElementById("total-calc").textContent = "$ " +  calculate(cost, tip)[1];
+      if (tip === 1){
+        addDouche();
+      }else{
+        removeDouche();
+      }
+    }
+};
+
+textInput.addEventListener("keyup", printScreen);
+highlight.addEventListener("mouseout", printScreen);
