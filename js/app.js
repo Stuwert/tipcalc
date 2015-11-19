46f0d7c641ca8cd2b8c1b145c2037525f71bf7a8
Stuwert <stuart@urback.net>
2015-11-18 19:51:42 -0700

    Finalized but ugly

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
-var myButton = document.getElementById("awesome");
-console.log(myButton);
-myButton.addEventListener("click", function(){
-  var number = myButton.previousElementSibling.value;
-  number = Number(number);
-  var totalCalc = document.getElementById("tip-calc");
-  var tip = .20 * number;
-  tip = Number(tip);
-  var totalAmt = document.getElementById("total-calc");
-  totalCalc.textContent = "$ " + tip;
-  totalAmt.textContent = "$ " + (tip + number);
+var awesome = document.getElementById("awesome");
+var douche = document.getElementById("douche");
+var dece = document.getElementById("dece");
+var poor = document.getElementById("poor");
+
+douche.addEventListener("click", function(){
+  document.getElementById('header').innerHTML = "You're a Douche";
+  document.getElementById('main').setAttribute('class', 'red');
 });
+
+
+function clickButton (buttonToClick, percent){
+  buttonToClick.addEventListener("click", function(){
+    var number = +document.getElementById("mealcost").value;
+    var totalCalc = document.getElementById("tip-calc");
+    var tip = +percent*number;
+    var totalAmt = document.getElementById("total-calc");
+    var total = tip + number;
+    total = total.toFixed(2);
+    tip = tip.toFixed(2);
+    totalCalc.textContent = "$ " + tip;
+    totalAmt.textContent = "$ " + total;
+    if (buttonToClick != douche){
+      document.getElementById('main').removeAttribute("class", "red");
+      document.getElementById('header').innerHTML = "Tip Calculator";
+    }
+  });
+}
+
+clickButton(awesome, .20);
+clickButton(douche, .05);
+clickButton(dece, .15);
+clickButton(poor, .1);
